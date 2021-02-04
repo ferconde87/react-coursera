ec2fce81a57801d2f9590aa7578e84cd032122a2
fer <ferconde87@gmail.com>
2021-02-04 17:46:09 +0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -90,7 +91,7 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
 	return (
 		<Card>
-			<CardImg width="100%" object src={dish.image} alt={dish.name} />
+			<CardImg width="100%" object src={baseUrl + dish.image} alt={dish.name} />
 			<CardBody>
 				<CardTitle>{dish.name}</CardTitle>
 				<CardText>{dish.description}</CardText>
