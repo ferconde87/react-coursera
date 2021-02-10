5261670f778e7e8fdec41f992b2e3cf71e66eb44
fer <ferconde87@gmail.com>
2021-02-11 00:26:49 +0700

    Assignment 4

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -38,8 +38,8 @@ function Home(props) {
       <div className="row align-items-start">
         <div className="col-12 col-md m-1">
           <RenderCard item={props.dish}
-            isLoading={props.dishesLoading}
-            errMess={props.dishesErrMess} />
+            isLoading={props.dishLoading}
+            errMess={props.dishErrMess} />
         </div>
         <div className="col-12 col-md m-1">
           <RenderCard item={props.promotion}
@@ -47,7 +47,10 @@ function Home(props) {
             errMess={props.promoErrMess} />
         </div>
         <div className="col-12 col-md m-1">
-          <RenderCard item={props.leader} />
+          <RenderCard item={props.leader} 
+            isLoading={props.leaderLoading}
+            errMess={props.leaderErrMess}
+          />
         </div>
       </div>
     </div>
